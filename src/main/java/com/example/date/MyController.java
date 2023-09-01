c1c40ccced2a5ae55a59fd9789a230fc516d80b9
ajay <nerellaajay98@gmail.com>
2023-09-01 08:53:18 +0000

    solution

--- src/main/java/com/example/date/MyController.java
+++ src/main/java/com/example/date/MyController.java
@@ -1,13 +1,17 @@
 package com.example.date;
 
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Date;
 
 @RestController
+
 
 public class MyController {
     @GetMapping("/")
     public String getDate(){
-        return "31-Aug-2023";
+        Date date = new Date()
+        return date;
     }
 }
